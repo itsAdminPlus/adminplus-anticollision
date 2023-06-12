6f6ec7c2a7930852297af1b967cc37fcc97633d7
AdminPlus <89868900+itsAdminPlus@users.noreply.github.com>
2023-06-12 08:10:57 -0400

    Add files via upload

---
+++ cl_main.lua
@@ -0,0 +1,88 @@
+local entityEnumerator = {
+    __gc = function(enum)
+        if enum.destructor and enum.handle then
+            enum.destructor(enum.handle)
+        end
+
+        enum.destructor = nil
+        enum.handle = nil
+    end
+}
+
+local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+    return coroutine.wrap(function()
+        local iter, id = initFunc()
+        if not id or id == 0 then
+            disposeFunc(iter)
+            return
+        end
+
+        local enum = {handle = iter, destructor = disposeFunc}
+        setmetatable(enum, entityEnumerator)
+
+        local next = true
+        repeat
+        coroutine.yield(id)
+        next, id = moveFunc(iter)
+        until not next
+
+        enum.destructor, enum.handle = nil, nil
+        disposeFunc(iter)
+    end)
+end
+
+function EnumerateObjects()
+    return EnumerateEntities(FindFirstObject, FindNextObject, EndFindObject)
+end
+
+local props = {
+    "prop_traffic_01a",
+    "prop_traffic_01b",
+    "prop_traffic_01d",
+    "prop_traffic_03a",
+    "prop_traffic_03b",
+    "prop_traffic_lightset_01",
+    "prop_ind_light_01a",
+    "prop_ind_light_01b",
+    "prop_ind_light_01c",
+    "prop_ind_light_02a",
+    "prop_ind_light_02b",
+    "prop_ind_light_01c",
+    "prop_ind_light_02a",
+    "prop_ind_light_02b",
+    "prop_ind_light_02c",
+    "prop_ind_light_03a",
+    "prop_ind_light_03b",
+    "prop_ind_light_03c",
+    "prop_ind_light_04",
+    "prop_streetlight_01",
+    "prop_streetlight_02",
+    "prop_streetlight_03",
+    "prop_streetlight_03b",
+    "prop_streetlight_03c",
+    "prop_streetlight_03d",
+    "prop_streetlight_03e",
+    "prop_streetlight_14a",
+    "prop_streetlight_15a",
+    "prop_streetlight_04",
+}
+
+Citizen.CreateThread(function()
+    local propsHash = {}
+    local veh = GetVehiclePedIsIn(PlayerPedId())
+    for i=1,#props do
+        propsHash[GetHashKey(props[i])] = true
+        --print (GetHashKey(props[i]))
+    end
+    while true do
+        Citizen.Wait(500)
+        for v in EnumerateObjects() do
+            if propsHash[GetEntityModel(v)] then
+                --FreezeEntityPosition(v, true)
+                --SetEntityCanBeDamaged(v, false)
+                SetEntityCollision(v, false)
+                --SetEntityNoCollisionEntity(veh, v, false)
+            end
+        end
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AdminPlus#0001 | AdminPlus'
+description 'AdminPlus Anti Collision For x Props'
+lua54 'yes'
+
+client_scripts {
+    'client/*'
+}
